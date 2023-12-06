ccc7b77bfe495546ab28e47cad6fdd7201458119
Saketh <reddysaketh980@gmail.com>
2023-12-05 20:34:38 -0600

    Day 4 part 1 and 2 strategy
    ### Part 1:
    
    1. **Understanding the Problem:**
       - Recognize that the problem involves scratchcards with two sets of numbers, and the goal is to find matches between them.
       - Understand that points are awarded for each match, with doubling points for subsequent matches.
    
    2. **Parsing the Input:**
       - Identify the structure of the input, where each line represents a scratchcard with winning and owned numbers.
    
    3. **Organizing Data:**
       - Consider creating a data structure to store the scratchcards, possibly a list of lists or tuples to represent each card.
    
    4. **Matching Numbers:**
       - Devise a method to find common numbers between the winning and owned sets on each scratchcard.
       - Understand that points are awarded based on the number of matches.
    
    5. **Calculating Points:**
       - Implement a mechanism to calculate points for each scratchcard.
       - Realize that the points are calculated based on the number of matches, with doubling for subsequent matches.
    
    6. **Accumulating Total Points:**
       - Develop a way to accumulate the total points as scratchcards are processed.
    
    7. **Final Output:**
       - Print or store the total points obtained from all the scratchcards.
    
    ### Part 2:
    
    1. **Reassessing Rules:**
       - Understand that Part 2 introduces a change in rules where winning scratchcards can yield more scratchcards.
       - Realize that each winning scratchcard produces copies of subsequent scratchcards.
    
    2. **Handling Copies:**
       - Consider how to manage the generation of copies for each winning scratchcard.
       - Understand that the process continues until no more scratchcards are won.
    
    3. **Iteration through Scratchcards:**
       - Plan how to iteratively process both the original scratchcards and their copies.
       - Realize that this process repeats until no more scratchcards are won.
    
    4. **Accumulating Total Scratchcards:**
       - Develop a mechanism to keep track of the total number of scratchcards obtained, including both originals and copies.
    
    5. **Final Output (Part 2):**
       - Print or store the total number of scratchcards obtained after processing all originals and copies.

---
+++ Day 4.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+
+# Read input file, strip extra whitespaces, and split into lines
+input_data = open("input.txt").read().strip()
+lines = input_data.split('\n')
+
+# Initialize variables
+total_points = 0
+accumulated_counts = defaultdict(int)
+
+# Process each line in the input
+for i, line in enumerate(lines):
+    # Update the count for the current line index
+    accumulated_counts[i] += 1
+
+    # Split the line into the first part and the rest
+    first_part, rest = line.split('|')
+
+    # Extract id and card numbers from the first part
+    identifier, card_part = first_part.split(':')
+    card_numbers = [int(x) for x in card_part.split()]
+
+    # Extract numbers from the rest part
+    rest_numbers = [int(x) for x in rest.split()]
+
+    # Calculate the common values between card and rest
+    common_values = len(set(card_numbers) & set(rest_numbers))
+
+    # Update total points based on common values
+    if common_values > 0:
+        total_points += 2**(common_values - 1)
+
+    # Update counts for subsequent lines
+    for j in range(common_values):
+        accumulated_counts[i + 1 + j] += accumulated_counts[i]
+
+# Print the results
+print('---Part 1---')
+print(total_points)
+print("--Part 2---")
+print(sum(accumulated_counts.values()))
